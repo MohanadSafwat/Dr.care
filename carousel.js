a24ddad2474515fb223eb3cc830e32d097f6754e
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2021-09-08 01:02:14 +0200

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,45 +1,45 @@
-// var currentSlide =1;
+var currentSlide =1;
 
-// function dotFunctionFirst(){
-//     currentSlide =2;
-//     currentSlideFunc();
-//     console.log(currentSlide);
-// }
+function dotFunctionFirst(){
+    currentSlide =2;
+    currentSlideFunc();
+    console.log(currentSlide);
+}
 
-// function dotFunctionSecond(){
-//     currentSlide =1;
-//     currentSlideFunc();
-//     console.log(currentSlide);
+function dotFunctionSecond(){
+    currentSlide =1;
+    currentSlideFunc();
+    console.log(currentSlide);
 
-// }
+}
 
-// function currentSlideFunc(){
-//     var slideDivOne = document.getElementById("slide-1");
-//     var slideDivTwo = document.getElementById("slide-2");
+function currentSlideFunc(){
+    var slideDivOne = document.getElementById("slide-1");
+    var slideDivTwo = document.getElementById("slide-2");
 
-//     if(currentSlide == 1){
-//         slideDivOne.style.display = "block";
-//         slideDivTwo.style.display = "none";
+    if(currentSlide == 1){
+        slideDivOne.style.display = "block";
+        slideDivTwo.style.display = "none";
 
-//     }
-//     if(currentSlide == 2){
-//         slideDivOne.style.display = "none";
-//         slideDivTwo.style.display = "block";
-//     }
+    }
+    if(currentSlide == 2){
+        slideDivOne.style.display = "none";
+        slideDivTwo.style.display = "block";
+    }
 
-// }
+}
 
 
 
-// setInterval(function(){ if(currentSlide == 1 ){
-//     currentSlide =2;
-//     currentSlideFunc();
-//     console.log(currentSlide);
-// }
-// else{
-//     currentSlide =1;
-//     console.log(currentSlide);
-//     currentSlideFunc();
+setInterval(function(){ if(currentSlide == 1 ){
+    currentSlide =2;
+    currentSlideFunc();
+    console.log(currentSlide);
+}
+else{
+    currentSlide =1;
+    console.log(currentSlide);
+    currentSlideFunc();
 
-// }
-// },5000);
+}
+},5000);
