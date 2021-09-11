4ce3fdf3db509a914a9f14de3dc23b52cde93fec
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2021-09-11 05:36:25 +0200

    sec-5 js

---
+++ sec-5.js
@@ -0,0 +1,46 @@
+function sec5in(order) {
+  if (order == 1) {
+    document.getElementById("sec5-1").style.display = "block";
+    document.getElementById("sec5-head1").style.color = "#007bff";
+
+  }
+  if (order == 2) {
+    document.getElementById("sec5-2").style.display = "block";
+    document.getElementById("sec5-head2").style.color = "#007bff";
+
+  }
+  if (order == 3) {
+    document.getElementById("sec5-3").style.display = "block";
+    document.getElementById("sec5-head3").style.color = "#007bff";
+
+  }
+  if (order == 4) {
+    document.getElementById("sec5-4").style.display = "block";
+    document.getElementById("sec5-head4").style.color = "#007bff";
+
+  }
+}
+
+function sec5out(order) {
+    if (order == 1) {
+      document.getElementById("sec5-1").style.display = "none";
+      document.getElementById("sec5-head1").style.color = "#000";
+
+    }
+    if (order == 2) {
+      document.getElementById("sec5-2").style.display = "none";
+      document.getElementById("sec5-head2").style.color = "#000";
+
+    }
+    if (order == 3) {
+      document.getElementById("sec5-3").style.display = "none";
+      document.getElementById("sec5-head3").style.color = "#000";
+
+    }
+    if (order == 4) {
+      document.getElementById("sec5-4").style.display = "none";
+      document.getElementById("sec5-head4").style.color = "#000";
+
+    }
+  }
+  
