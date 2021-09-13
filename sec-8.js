bfc63c583c7d3aca29accd20845bc168ea210a40
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2021-09-13 20:39:30 +0200

    sec-8 js

---
+++ sec-8.js
@@ -0,0 +1,62 @@
+function sec8in(order) {
+    if (order == 1) {
+    
+      document.getElementById("sec-8-price-1").style.color = "#ff8000";
+      document.getElementById("sec-8-button-1").style.backgroundColor = "#ff8000";
+      document.getElementById("sec-8-container-1").style.borderColor = "#ff8000";
+
+  
+    }
+    else if (order == 2) {
+  
+        document.getElementById("sec-8-price-2").style.color = "#ff8000";
+        document.getElementById("sec-8-button-2").style.backgroundColor = "#ff8000";
+        document.getElementById("sec-8-container-2").style.borderColor = "#ff8000";  
+  
+    }
+    else if (order == 3) {
+  
+        document.getElementById("sec-8-price-3").style.color = "#ff8000";
+        document.getElementById("sec-8-button-3").style.backgroundColor = "#ff8000";
+        document.getElementById("sec-8-container-3").style.borderColor = "#ff8000";  
+  
+    }
+    else if (order == 4) {
+  
+      document.getElementById("sec-8-price-4").style.color = "#ff8000";
+      document.getElementById("sec-8-button-4").style.backgroundColor = "#ff8000";
+      document.getElementById("sec-8-container-4").style.borderColor = "#ff8000";
+  
+  
+    }
+  }
+  
+  function sec8out(order) {
+      if (order == 1) {
+        document.getElementById("sec-8-price-1").style.color = "#2f89fc";
+        document.getElementById("sec-8-button-1").style.backgroundColor = "#2f89fc";
+        document.getElementById("sec-8-container-1").style.borderColor = "#2f89fc";  
+  
+      }
+      else if (order == 2) {
+  
+        document.getElementById("sec-8-price-2").style.color = "#2f89fc";
+        document.getElementById("sec-8-button-2").style.backgroundColor = "#2f89fc";
+        document.getElementById("sec-8-container-2").style.borderColor = "#2f89fc";  
+  
+      }
+      else if (order == 3) {
+  
+        document.getElementById("sec-8-price-3").style.color = "#2f89fc";
+        document.getElementById("sec-8-button-3").style.backgroundColor = "#2f89fc";
+        document.getElementById("sec-8-container-3").style.borderColor = "#2f89fc";  
+  
+      }
+      else if (order == 4) {
+  
+        document.getElementById("sec-8-price-4").style.color = "#2f89fc";
+        document.getElementById("sec-8-button-4").style.backgroundColor = "#2f89fc";
+        document.getElementById("sec-8-container-4").style.borderColor = "#2f89fc";  
+  
+      }
+    }
